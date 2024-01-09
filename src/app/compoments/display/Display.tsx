3d274fb2b98677ddc28763ed259f6f1894129aa4
SantosFab <fabriciosantos.dev@gmail.com>
2024-01-09 15:40:48 -0300

    modifico o tipo da variavel currentView, por causa das operacoes decimais

--- src/app/compoments/display/Display.tsx
+++ src/app/compoments/display/Display.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 
 interface DisplayProps {
   historyView?: string;
-  currentView: number;
+  currentView: string;
 }
 
 const Display: FunctionComponent<DisplayProps> = ({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Button from "./compoments/button/Button";
 import Display from "./compoments/display/Display";
 
 export default function Calculator() {
-  const [currentView, setCurrentView] = useState<number>(0);
+  const [currentView, setCurrentView] = useState<string>('0');
   const [symbol, setSymbol] = useState<string | undefined>(undefined);
   const [currentHistory, setCurrentHistory] = useState<string | undefined>(
     undefined
@@ -12,40 +12,41 @@ export default function Calculator() {
 
   function clear(): void {
     setCurrentHistory(undefined);
-    setCurrentView(0);
+    setCurrentView('0');
   }
 
   function deleteNumber(): void {
     setCurrentView((current) => {
-      const currentAsString = current.toString();
+      
 
-      if (currentAsString.length > 1) {
-        const newNumber =
-          current < 0
-            ? -1 * Math.floor(Math.abs(current) / 10)
-            : Math.floor(current / 10);
-        return newNumber;
+      if (current.length > 1) {
+        
+        return current.slice(0, -1);
       } else {
-        return 0;
+        return '0';
       }
     });
   }
 
   function addNumberOnCurrentView(number: number): void {
     setCurrentView((current) => {
-      const isfifteen = current.toString();
-      if (isfifteen.length >= 15) {
+      if (current.length >= 15) {
         return current;
       }
-      return current * 10 + number;
+      return (parseFloat(current) * 10 + number).toString();
     });
   }
 
-  function addOperation(symbol: string):void {
+  function addOperation(symbol: string): void {
     setSymbol(symbol);
-    setCurrentHistory(currentView + symbol)
-    setCurrentView(0)
+    setCurrentHistory(currentView + symbol);
+    setCurrentView('0');
   }
+
+  function addFraction(): void {
+   
+  }
+  
 
   useEffect(() => {
     console.log("the symbol is:", symbol);
@@ -56,25 +57,25 @@ export default function Calculator() {
       <Display currentView={currentView} historyView={currentHistory}></Display>
       <Button symbol="CE" isSpan={true} isGray={true} onClick={() => clear()} />
       <Button symbol="⇤" isGray={true} onClick={() => deleteNumber()} />
-      <Button symbol="/" isGray={true} onClick={() => addOperation('/')} />
+      <Button symbol="/" isGray={true} onClick={() => addOperation("/")} />
       <Button symbol={7} onClick={() => addNumberOnCurrentView(7)} />
       <Button symbol={8} onClick={() => addNumberOnCurrentView(8)} />
       <Button symbol={9} onClick={() => addNumberOnCurrentView(9)} />
-      <Button symbol="*" isGray={true} onClick={() => addOperation('*')} />
+      <Button symbol="*" isGray={true} onClick={() => addOperation("*")} />
       <Button symbol={4} onClick={() => addNumberOnCurrentView(4)} />
       <Button symbol={5} onClick={() => addNumberOnCurrentView(5)} />
       <Button symbol={6} onClick={() => addNumberOnCurrentView(6)} />
-      <Button symbol="-" isGray={true} onClick={() => addOperation('-')} />
+      <Button symbol="-" isGray={true} onClick={() => addOperation("-")} />
       <Button symbol={1} onClick={() => addNumberOnCurrentView(1)} />
       <Button symbol={2} onClick={() => addNumberOnCurrentView(2)} />
       <Button symbol={3} onClick={() => addNumberOnCurrentView(3)} />
-      <Button symbol="+" isGray={true} onClick={() => addOperation('+')} />
+      <Button symbol="+" isGray={true} onClick={() => addOperation("+")} />
       <Button
         symbol={0}
         isSpan={true}
         onClick={() => addNumberOnCurrentView(0)}
       />
-      <Button symbol="." />
+      <Button symbol="." onClick={() => addFraction()} />
       <Button symbol="=" isBlue={true} />
     </div>
   );
