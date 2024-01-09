4df3555ce23f7dc9e286a372faf7427f88357fac
SantosFab <fabriciosantos.dev@gmail.com>
2024-01-09 14:38:34 -0300

    implemento parcialmente logica de operacao matematica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Button from "./compoments/button/Button";
 import Display from "./compoments/display/Display";
 
 export default function Calculator() {
-  const [currentView, setCurrentView] = useState<number>(-12);
+  const [currentView, setCurrentView] = useState<number>(0);
   const [symbol, setSymbol] = useState<string | undefined>(undefined);
   const [currentHistory, setCurrentHistory] = useState<string | undefined>(
     undefined
@@ -41,6 +41,12 @@ export default function Calculator() {
     });
   }
 
+  function addOperation(symbol: string):void {
+    setSymbol(symbol);
+    setCurrentHistory(currentView + symbol)
+    setCurrentView(0)
+  }
+
   useEffect(() => {
     console.log("the symbol is:", symbol);
   }, [symbol]);
@@ -50,19 +56,19 @@ export default function Calculator() {
       <Display currentView={currentView} historyView={currentHistory}></Display>
       <Button symbol="CE" isSpan={true} isGray={true} onClick={() => clear()} />
       <Button symbol="⇤" isGray={true} onClick={() => deleteNumber()} />
-      <Button symbol="/" isGray={true} onClick={() => setSymbol("/")} />
+      <Button symbol="/" isGray={true} onClick={() => addOperation('/')} />
       <Button symbol={7} onClick={() => addNumberOnCurrentView(7)} />
       <Button symbol={8} onClick={() => addNumberOnCurrentView(8)} />
       <Button symbol={9} onClick={() => addNumberOnCurrentView(9)} />
-      <Button symbol="*" isGray={true} onClick={() => setSymbol("*")} />
+      <Button symbol="*" isGray={true} onClick={() => addOperation('*')} />
       <Button symbol={4} onClick={() => addNumberOnCurrentView(4)} />
       <Button symbol={5} onClick={() => addNumberOnCurrentView(5)} />
       <Button symbol={6} onClick={() => addNumberOnCurrentView(6)} />
-      <Button symbol="-" isGray={true} onClick={() => setSymbol("-")} />
+      <Button symbol="-" isGray={true} onClick={() => addOperation('-')} />
       <Button symbol={1} onClick={() => addNumberOnCurrentView(1)} />
       <Button symbol={2} onClick={() => addNumberOnCurrentView(2)} />
       <Button symbol={3} onClick={() => addNumberOnCurrentView(3)} />
-      <Button symbol="+" isGray={true} onClick={() => setSymbol("+")} />
+      <Button symbol="+" isGray={true} onClick={() => addOperation('+')} />
       <Button
         symbol={0}
         isSpan={true}
