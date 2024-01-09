732b49779bb771f814f80b83645e853a372dc878
SantosFab <fabriciosantos.dev@gmail.com>
2024-01-09 08:37:32 -0300

    crio compomente button

---
+++ src/app/compoments/button/Button.tsx
@@ -0,0 +1,36 @@
+import { FunctionComponent, MouseEvent } from "react";
+import styles from "./styles.module.css";
+
+interface ButtonProps {
+  symbol: String | number;
+  onClick?: (event: MouseEvent<HTMLDivElement>) => void;
+  isSpan?: boolean;
+  isBlue?: boolean;
+  isGray?: boolean;
+  isGrayLight?: boolean;
+}
+
+const Button: FunctionComponent<ButtonProps> = ({
+  symbol,
+  onClick,
+  isSpan = false,
+  isBlue = false,
+  isGray = false,
+  isGrayLight = false,
+}) => {
+  return (
+    <div
+      className={` flex justify-center items-center 
+      ${styles.button} 
+      ${isSpan && "col-span-2"} 
+      ${isBlue && styles.blue} 
+      ${isGray && styles.gray}
+      `}
+      onClick={onClick}
+    >
+      {symbol}
+    </div>
+  );
+};
+
+export default Button;
