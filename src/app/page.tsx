b6027a9ab9978bb8081f53c5ce0941d0fc3a10e7
SantosFab <fabriciosantos.dev@gmail.com>
2024-01-11 09:25:08 -0300

    implemento logica de operacoes matematica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Display from "./compoments/display/Display";
 
 export default function Calculator() {
   const [currentView, setCurrentView] = useState<string>("0");
-  const [symbol, setSymbol] = useState<string | undefined>(undefined);
   const [currentHistory, setCurrentHistory] = useState<string | undefined>(
     undefined
   );
@@ -61,6 +60,140 @@ export default function Calculator() {
       setCurrentView((current) => currentView + ".");
     }
     return;
+  }
+
+  function handlerOperator(operation: TypeOfOperator): void {
+    if (
+      currentHistory === undefined &&
+      symbol === undefined &&
+      firstTerm === undefined &&
+      resultOperation === undefined
+    ) {
+      setSymbol(operation);
+      setFirstTerm(parseFloat(currentView));
+      setCurrentHistory(currentView + operation.operator);
+      setCurrentView("0");
+    } else if (
+      currentView !== "0" &&
+      currentHistory !== undefined &&
+      symbol !== undefined &&
+      firstTerm !== undefined &&
+      !handlerSplitStringByOperators()
+    ) {
+      handlerResultOfTheOperation({ operation });
+      setSymbol(operation);
+    } else {
+      setSymbol(operation);
+      resultOperation === undefined
+        ? setCurrentHistory(firstTerm + operation.operator)
+        : setCurrentHistory(resultOperation + operation.operator);
+    }
+  }
+
+  function handlerResultOfTheOperation({
+    operation,
+    isResult = false,
+  }: {
+    operation?: TypeOfOperator;
+    isResult?: boolean;
+  }): void {
+    let result: number;
+    let parseCurrentView: number = parseFloat(currentView);
+    if (
+      isResult &&
+      resultOperation === undefined &&
+      firstTerm !== undefined &&
+      symbol !== undefined &&
+      !handlerSplitStringByOperators()
+    ) {
+      result = handlerMathematicalOperationa(
+        firstTerm,
+        symbol,
+        currentView === "0" ? firstTerm : parseCurrentView
+      );
+      setCurrentHistory(
+        `${firstTerm} ${symbol.operator} ${
+          currentView === "0" ? firstTerm : parseCurrentView
+        } = ${result}`
+      );
+      setResultOperation(result);
+      setCurrentView("0");
+    } else if (
+      operation !== undefined &&
+      currentView !== "0" &&
+      firstTerm !== undefined &&
+      symbol !== undefined
+    ) {
+      result = handlerMathematicalOperationa(
+        resultOperation === undefined ? firstTerm : resultOperation,
+        symbol,
+        parseCurrentView
+      );
+      setResultOperation(result);
+      setSymbol(operation);
+      setCurrentHistory(result + operation.operator);
+      setCurrentView("0");
+    } else if (
+      isResult &&
+      currentView === "0" &&
+      resultOperation !== undefined &&
+      symbol !== undefined &&
+      firstTerm !== undefined &&
+      handlerSplitStringByOperators()
+    ) {
+      result = handlerMathematicalOperationa(resultOperation, symbol, firstTerm);
+      setCurrentHistory(
+        `${resultOperation} ${symbol.operator} ${firstTerm} = ${result}`
+      );
+      setResultOperation(result);
+    } else if (
+      isResult &&
+      currentView !== "0" &&
+      resultOperation !== undefined &&
+      symbol !== undefined
+    ) {
+      result = handlerMathematicalOperationa(
+        resultOperation,
+        symbol,
+        parseCurrentView
+      );
+      setCurrentHistory(
+        `${resultOperation} ${symbol.operator} ${parseCurrentView} = ${result}`
+      );
+      setFirstTerm(parseCurrentView);
+      setCurrentView("0");
+      setResultOperation(result);
+    }
+  }
+
+  function handlerMathematicalOperationa(
+    numberOne: number,
+    { operator }: TypeOfOperator,
+    numberTwo: number
+  ): number {
+    switch (operator) {
+      case "/":
+        return numberOne / numberTwo;
+      case "*":
+        return numberOne * numberTwo;
+      case "-":
+        return numberOne - numberTwo;
+      case "+":
+        return numberOne + numberTwo;
+      default:
+        throw new Error("Invalid operator");
+    }
+  }
+
+  function handlerSplitStringByOperators(): boolean {
+    if (!currentHistory) {
+      return false;
+    }
+    const result = currentHistory
+      .split(/\/|\*|\-|\+|=/g)
+      .map((item) => parseFloat(item))
+      .filter((num) => !isNaN(num));
+    return result.length > 2;
   }
 
   return (
