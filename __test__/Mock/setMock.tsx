f4972078edfefce0e8a31b659a2b89513b35d137
SantosFab <fabriciosantos.dev@gmail.com>
2024-01-15 17:42:16 -0300

    organizo arquivo

--- __test__/Mock/setMock.tsx
+++ __test__/Mock/setMock.tsx
@@ -4,13 +4,17 @@ import { Dispatch } from "react";
 export const setCurrentHistoryMock = jest.fn() as jest.Mock<
   Dispatch<string | undefined>
 >;
+
 export const setCurrentViewMock = jest.fn() as jest.Mock<Dispatch<string>>;
+
 export const setSymbolMock = jest.fn() as jest.Mock<
   Dispatch<TypeOfOperator | undefined>
 >;
-export const setFirtTermMock = jest.fn() as jest.Mock<
+
+export const setFirstTermMock = jest.fn() as jest.Mock<
   Dispatch<number | undefined>
 >;
+
 export const setResultOperationMock = jest.fn() as jest.Mock<
   Dispatch<number | undefined>
 >;
