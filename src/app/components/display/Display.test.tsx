6e39b480db962a51f77d35c857d6cce2131265d4
SantosFab <fabriciosantos.dev@gmail.com>
2024-01-15 14:07:50 -0300

    teste do componente display

---
+++ src/app/components/display/Display.test.tsx
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom';
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display";
+
+describe("Display component", () => {
+  it("renderização com somente o CurrentView", () => {
+    const currentView = "Conteúdo do Current View";
+
+    const { getByText, queryByText } = render(
+      <Display currentView={currentView} />
+    );
+
+    expect(getByText(currentView)).toBeInTheDocument();
+    const historyViewElement = queryByText(/conteúdo do history view/i);
+    expect(historyViewElement).toBeNull();
+  });
+
+  it("renderização com o CurrentView e CurrentHistory", () => {
+    const historyView = "Conteúdo do History View";
+    const currentView = "Conteúdo do Current View";
+
+    const { getByText } = render(
+      <Display historyView={historyView} currentView={currentView} />
+    );
+
+    expect(getByText(historyView)).toBeInTheDocument();
+    expect(getByText(currentView)).toBeInTheDocument();
+  });
+});
